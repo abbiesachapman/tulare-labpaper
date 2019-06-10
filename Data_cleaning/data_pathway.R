655cdd047382c0eb4d549d43e6ae5394aea33176
Lauren Hallett <hallett@uoregon.edu>
2019-06-10 15:00:46 -0700

    to set datapathways for folks with Mac vs PC

---
+++ Data_cleaning/data_pathway.R
@@ -0,0 +1,6 @@
+## Set data pathways! Do this manually, the first pathway for Macs, the second for PCs
+
+# Do this at the beginning of the workflow and the rest should be all standardized
+
+datpath <- "~/University of Oregon/O365.hallett-lab - Documents/Tulare-LabPaper/Serpentine_Plants2001_2018_MK_working.xlsx"
+
