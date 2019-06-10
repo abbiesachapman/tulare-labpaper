5e9699c8ecd1da0b304b02874553889f56dd257b
Lauren Hallett <hallett@uoregon.edu>
2019-06-10 15:13:26 -0700

    2003 cleaning and a little tidying of 2001

--- Data_cleaning/dataformatting_2003.R
+++ Data_cleaning/dataformatting_2003.R
@@ -5,32 +5,31 @@ library(stringr)
 ## GOPHER, COWPIE, BARE, LITTER
 
 dat <- read_excel(datpath, 
-                  sheet = "Plants2001", skip = 3)
+                  sheet = "Plants2003", skip = 3) 
 
 datnames <- read_excel(datpath, 
-                       sheet = "Plants2001")[0,]
+                       sheet = "Plants2003")[0,]
 
 names(dat) = names(datnames)
-names(dat)[1:2] = c("quadrat", "site")
+names(dat)[1:3] = c("quadrat", "quadrat1", "site")
 
 
-dat2001env <- dat %>%
+dat2003env <- dat %>%
   select(quadrat, site, GOPHER, BARE, ROCK, LITTER, COWPIE)
 
-dat2001 <- dat %>%
-  select(-c(GOPHER, BARE, ROCK, LITTER, COWPIE)) %>%
-  gather(species, cover, "X__3":"X__43") %>%
+dat2003 <- dat %>%
+  select(-c(GOPHER, BARE, ROCK, LITTER, COWPIE, quadrat1)) %>%
+  gather(species, cover, "X__4":"X__57") %>%
   mutate(dummyspp = substr(species, 1,3)) %>%
   filter(dummyspp != "X__") %>%
   select(-dummyspp) %>%
   mutate(site = substr(quadrat, 1,2)) %>%
   filter(site == "TH") %>%
-  mutate(year = 2001)
+  mutate(year = 2003)
 
 
-key2001 <- dat2001 %>%
+key2003 <- dat2003 %>%
   select(quadrat, site, year) %>%
   unique()
 
-rm(dat, datnames)
-
+rm(dat, dat2, datnames)
