c9bb44ff6e0760649f9725293ee297935cc45c5c
Lauren Hallett <hallett@uoregon.edu>
2019-06-10 16:09:44 -0700

    2007 cleaning and edits to 2006

--- Data_cleaning/dataformatting_2007.R
+++ Data_cleaning/dataformatting_2007.R
@@ -5,30 +5,29 @@ library(stringr)
 ## GOPHER, COWPIE, BARE, LITTER
 
 dat <- read_excel(datpath, 
-                  sheet = "Plants 2006", skip = 1) 
+                  sheet = "Plants2007", skip = 1) 
 
 datnames <- read_excel(datpath, 
-                       sheet = "Plants 2006")[0,]
+                       sheet = "Plants2007")[0,]
 
 names(dat) = names(datnames)
-names(dat)[1:6] = c("year", "Page", "OddEven", "quadrat","transect", "site")
+names(dat)[1:4] = c("year", "quadrat","transect", "site")
 
 
-dat2006env <- dat %>%
+dat2007env <- dat %>%
   select(quadrat, site, transect, year, GOPHER, BARE, ROCK, LITTER, COWPIE)
 
-dat2006 <- dat %>%
-  select(-c(GOPHER, BARE, ROCK, LITTER, COWPIE, Page, OddEven)) %>%
-  gather(species, cover, "X__7":"X__77") %>%
+dat2007 <- dat %>%
+  select(-c(GOPHER, BARE, ROCK, LITTER, COWPIE)) %>%
+  gather(species, cover, "X__5":"X__36") %>%
   mutate(dummyspp = substr(species, 1,3)) %>%
   filter(dummyspp != "X__") %>%
   select(-dummyspp) %>%
   # mutate(site = substr(quadrat, 1,2)) %>%
-  filter(site == "TH-UBUG" | site == "TH-BG" | site == "TH-BUG") %>%
-  mutate(year = 2006)
+  filter(site == "TH"| site == "TH-BUG") 
 
 
-key2006 <- dat2006 %>%
+key2007 <- dat2007 %>%
   select(quadrat, site, year, transect) %>%
   unique()
 
