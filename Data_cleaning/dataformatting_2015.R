a6d6bf84cb4c9592645eba135d7aa45fda323418
Eliza Hernandez <elizahrndez@gmail.com>
2019-06-10 21:30:23 -0700

    Formatted data for 2015

---
+++ Data_cleaning/dataformatting_2015.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(readxl)
+library(stringr)
+
+## GOPHER, COWPIE, BARE, LITTER
+dat <- read_excel(datpath, 
+                  sheet = "Plants2015", skip = 2) 
+
+datnames <- read_excel(datpath, 
+                       sheet = "Plants2015")[0,]
+
+names(dat) = names(datnames)
+names(dat)[4:6] = c("quadrat", "transect", "site")
+
+
+dat2015env <- dat %>%
+  select(quadrat, transect, site, GOPHER, BARE, ROCK, LITTER, COWPIE) %>%
+  mutate(year = 2015)
+ 
+
+dat2015 <- dat %>%
+  select(-c(GOPHER, BARE, ROCK, LITTER, COWPIE, X__1, X__2, X__3)) %>%
+  gather(species, cover, "X__7":"X__29") %>%
+  mutate(dummyspp = substr(species, 1,3)) %>%
+  filter(dummyspp != "X__") %>%
+  select(-dummyspp) %>%
+  #mutate(site = substr(quadrat, 1,2)) %>%
+  filter(site =="TH" | site =="TH-MEC" | site == "TH-North") %>%
+  mutate(year = 2015)
+
+
+key2015 <- dat2015 %>%
+  select(transect, quadrat, site, year) %>%
+  unique()
+
+rm(dat, datnames)
