f29d9735ef04edacbcf98f64510ead26575c36bb
Lauren Hallett <hallett@uoregon.edu>
2019-07-26 11:30:32 -0700

    to do list for the next data compiling steps

--- Data_cleaning/Data_formatting.R
+++ Data_cleaning/Data_formatting.R
@@ -1,1 +1,6 @@
-## start of data cleaning 
+## Set data pathways! Do this manually, the first pathway for Macs, the second for PCs
+
+# Do this at the beginning of the workflow and the rest should be all standardized
+
+datpath <- "~/University of Oregon/O365.hallett-lab - Documents/Tulare-LabPaper/Serpentine_Plants2001_2018_MK_working.xlsx"
+
--- Data_cleaning/data_compiling.R
+++ Data_cleaning/data_compiling.R
@@ -21,6 +21,22 @@ source("Data_cleaning/dataformatting_2017.R")
 source("Data_cleaning/dataformatting_2018.R")
 
 
+# append all the data 
+alldat <- rbind(dat2001, dat2002, dat2003)
+
+## game plan with this: 
+# append all the years of data
+# add the thermal column to the quadrat master
+# merge in the quadrat master - this will give the treatments and the unique plot code
+# select only the burned grazed plots 
+# then link to the species key!
+## one way to check that you have all the species, is to make a data frame of all the unique specie in alldat
+## do a full join with the species key code - and look for gaps
+
+
+
+
+## old code
 unique(key2001$quadrat)
 unique(key2002$quadrat)
 
