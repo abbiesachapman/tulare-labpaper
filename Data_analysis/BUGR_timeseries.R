0d3d8d37a9df976789c1692cd051ed9e4d6a97e9
Lina Aoyama <laoyama@uoregon.edu>
2019-07-30 14:46:27 -0700

    timeseries plot

--- Data_analysis/BUGR_timeseries.R
+++ Data_analysis/BUGR_timeseries.R
@@ -2,6 +2,20 @@
 
 library(tidyverse)
 library(readr)
+library(ggplot2)
 
 dat <- read_csv(paste(datpath_clean, "/bugrdat.csv", sep=""))
 
+
+View(dat)
+
+richness <- dat %>%
+  filter(cover != 0, spname != c("Unknown", "Moss")) %>%
+  group_by(year, quadratNew) %>%
+  summarize(richness = length(unique(spname))) %>%
+  group_by(year) %>%
+  summarize(mean_rich = mean(richness))
+
+ggplot(richness, aes(year, mean_rich)) +
+  geom_point() +
+  geom_line()
