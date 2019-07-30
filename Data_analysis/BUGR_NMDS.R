97029569caf291cd10a96c3841546819c9a14956
Ashley Shaw <e.ashley.shaw@gmail.com>
2019-07-30 16:34:21 -0700

    wide df, bray-curtis, run NMDS

--- Data_analysis/BUGR_NMDS.R
+++ Data_analysis/BUGR_NMDS.R
@@ -2,6 +2,91 @@
 
 library(tidyverse)
 library(readr)
+library(vegan)
+library(MASS)
+library(tidyverse)
+library(dplyr)
+library(RVAideMemoire) #for posthoc tests on permanova
 
+# Import csv file, transform to wide data, call it dat
 dat <- read_csv(paste(datpath_clean, "/bugrdat.csv", sep=""))
+dat2<-dat %>% mutate(cover=cover+0.00001)
+data<- dat %>% dplyr::select(-X1, -spcode) %>% spread(spname, cover)
+data[is.na(data)] <- 0.00001
+levels(as.factor(dat$quadratNew))
+
+levels(as.factor(dat$thermal))
+
+str(data)
+
+
+data$ID <- seq.int(nrow(data))
+plotnames<-data[,1]
+cover.Bio<- data %>% dplyr::select(-c(1:4)) #wide data with ID columns removed, only species/cover for NMDS
+rownames(cover.Bio)<-plotnames
+#check for empty rows
+
+cover.Biodrop<-cover.Bio[rowSums(cover.Bio[, (1:157)]) ==0, ] #no empty rows, next step not needed
+cover.Biodrop<-cover.Bio[rowSums(cover.Bio[, (1:157)])  >0 ]#remove empty rows
+
+
+#if needed, relativize by row or colum or calculate presence/absence
+cover.rowsums <- rowSums(cover.Bio [1:157])
+cover.relrow <- data.frame(cover.Bio /cover.rowsums)
+#cover.colmax<-sapply(cover.Bio ,max)
+#cover.relcolmax <- data.frame(sweep(cover.Bio ,2,cover.colmax,'/'))
+#cover.pa <- cover.Bio %>% mutate_each(funs(ifelse(.>0,1,0)), 1:57)
+
+
+######################
+#2. NMS
+#see also section 2.1 of vegan tutorial: 
+#http://cc.oulu.fi/~jarioksa/opetus/metodi/vegantutor.pdf
+######################
+
+#make bray-curtis dissimilarity matrix
+spp.bcd <- vegdist(cover.relrow)
+
+#run NMS ordination
+spp.mds0 <-isoMDS(spp.bcd) #runs nms only once
+spp.mds0  #by default 2 dimensions returned, stress is 6.41
+ordiplot(spp.mds0)
+
+#prefer to run multiple NMS ordinations
+#with different starting configurations, and choose the best
+#this function does that, and in addition does several other steps too
+#including: 
+#standardizing the data (though fuction call below turns this off with autotransform=F)
+#calculating distance matrix (default bray-curtis)
+#running NMDS with random starts
+#rotation of axes to maximize variance of site scores on axis 1
+#calculate species scores based on weighted averaging
+
+help(metaMDS)
+spp.mds<-metaMDS(cover.relrow, trace = TRUE, autotransform=F, trymax=100, k=4) #runs several with different starting configurations
+#trace= TRUE will give output for step by step what its doing
+#default is 2 dimensions, can put k=4 for 4 dimensions
+spp.mds #solution converged after 49 tries, stress = 3.0
+summary(spp.mds)
+
+#plot results
+stressplot(spp.mds, spp.bcd) #stressplot to show fit, fit is decent
+ordiplot(spp.mds)
+spscores1<-scores(spp.mds,display="sites",choices=1)
+spscores2<-scores(spp.mds,display="sites",choices=2)
+tplots<-data[,4]
+tplot_levels<-levels(tplots)
+spscoresall<-data.frame(tplots,spscores1,spscores2)
+
+#plots colored based on treatment #help(ordiplot)
+bio.plot <- ordiplot(spp.mds,choices=c(1,2), type = "none")   #Set up the plot
+#cols1 <- rep(c("Red","Orange","purple", "black"), each = 18) #color based on drought treatment
+#Lcols <- rep(c("Red", "Orange", "purple", "black"))
+#shapes <- rep(c(15, 3, 17, 19, 5), each=3) #shapes on subplot
+#Lshapes <- rep(c(15,3,17,19,5))
+points(spscoresall$NMDS1,spscoresall$NMDS2,col=cols1,pch=12) 
+#text(spp.mds, display = "species", cex=0.5, col="grey30") #label species
+#legend("bottomright",legend=levels(Treatment), col=Lcols, pch=15, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
+# add legend for treatment
+#legend("topright",legend=levels(data$subplot), col="black", pch=Lshapes, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
 
