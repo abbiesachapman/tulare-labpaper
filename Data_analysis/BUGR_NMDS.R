82d8f929bd92583e75c467314a43ad0cdf218ed9
Eliza Hernandez <elizahrndez@gmail.com>
2019-08-08 12:14:25 -0700

    copy and pasted code from previous NMDS code to clean up data to look at moderate plots individually for NMDS

--- Data_analysis/BUGR_NMDS.R
+++ Data_analysis/BUGR_NMDS.R
@@ -11,8 +11,6 @@ library(RVAideMemoire) #for posthoc tests on permanova
 dat <- read_csv(paste(datpath_clean, "/bugrdat.csv", sep=""))
 dat<-as.data.frame(dat)
 #dat2<-dat %>% mutate(cover=cover+0.00001) #a work around for data with lots of zeros
-
-#convert to wide data
 data<- dat %>% dplyr::select(-X1, -spcode) %>% spread(spname, cover)
 data[is.na(data)] <- 0 #replace NAs with 0 (species not counted in plots have NAs when wide dataset created)
 levels(as.factor(dat$quadratNew))
@@ -36,7 +34,6 @@ data %>%
 
 str(data)
 
-#use full wide data, separate species only
 data$ID <- seq.int(nrow(data))
 plotnames<-data[,1]
 cover.Bio<- data %>% dplyr::select(-c(1:4)) #wide data with ID columns removed, only species/cover for NMDS
@@ -56,7 +53,7 @@ cover.Biodrop<-cover.Bio[rowSums(cover.Bio[, (1:157)]) ==0, ] #no empty rows, ne
 
 
 ######################
-#2. NMDS
+#2. NMS
 #see also section 2.1 of vegan tutorial: 
 #http://cc.oulu.fi/~jarioksa/opetus/metodi/vegantutor.pdf
 ######################
@@ -69,7 +66,7 @@ spp.mds0 <-isoMDS(spp.bcd) #runs nms only once
 spp.mds0  #by default 2 dimensions returned, stress is 6.4, converged
 ordiplot(spp.mds0) #ugly
 
-#prefer to run multiple NMDS ordinations
+#prefer to run multiple NMS ordinations
 #with different starting configurations, and choose the best
 #this function does that, and in addition does several other steps too including: 
 #standardizing the data (though fuction call below turns this off with autotransform=F)
@@ -127,70 +124,6 @@ text(spp.mds, display = "species", cex=0.5, col="grey30") #label species
 legend("bottomright",legend=levels(as.factor(cols1$thermal)), col=Lcols, pch=15, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
 legend("topright",legend=levels(as.factor(shapes$time)), col="black", pch=Lshapes, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
 
-
-#################
-#Subset data to look at early years only
-#-3/+3 years around the fire (2004)
-#################
-#First, create new data frame, select desired years
-early<-data %>% subset(year<=2007)
-#remove ID columns for NMDS ordinations
-cover.early<- early %>% dplyr::select(-c(1:4))
-#env variables for early
-early.env<-merge(env,early) %>% dplyr::select(NH4, NO3, totalN, ppt, temp)
-
-#make bray-curtis dissimilarity matrix
-early.bcd <- vegdist(cover.early)
-#run NMDS
-early.mds<-metaMDS(cover.early, trace = TRUE, autotransform=T, trymax=100, k=6) #runs several with different starting configurations
-#trace= TRUE will give output for step by step what its doing
-#default is 2 dimensions, can put k=4 for 4 dimensions
-early.mds #solution did not converge after 100 tries
-summary(early.mds)
-
-#quick plot of results
-stressplot(early.mds, early.bcd) #stressplot to show fit
-ordiplot(early.mds)
-
-#overlay environmental variables on full data
-envvec.early<-envfit(early.mds,early.env, na.rm=TRUE)
-envvec.early
-plot(early.mds)
-plot(envvec.early) #add vectors to previous ordination
-
-#store scores in new dataframe
-spscores1_early<-scores(early.mds,display="sites",choices=1)
-spscores2_early<-scores(early.mds,display="sites",choices=2)
-tplots_early<-early[,4]
-spscoresall_early<-data.frame(tplots_early,spscores1_early,spscores2_early)
-
-#make nicer plot colored based on thermal, shapes on pre/post fire
-#help(ordiplot)
-#first, set colors and shapes
-cols1e<- early %>% dplyr::select(thermal) %>% mutate(color = "black", 
-                                                   color = ifelse(thermal == "cool", "purple", 
-                                                                  ifelse(thermal=="moderate", "orange", 
-                                                                         ifelse(thermal=="very cool", "blue",
-                                                                                ifelse(thermal=="very warm", "red", color))))) #colors based on thermal group
-Lcolse <- rep(c("Black", "Purple", "Orange", "Blue", "Red")) #colors for the legend
-shapes.e <- data %>% dplyr::select(year) %>%
-  mutate(shape = 1, shape = ifelse(year == "2004", 8, 
-                                   ifelse(year>="2005", 16,shape))) #shapes based on year 
-shapes.e<-shapes.e %>% mutate(time="Pre-Fire (2001-2003)", 
-                          time= ifelse(year==2004, "Fire (2004)",
-                                       ifelse(year>=2005, "Post-Fire (2005-2007)",time))) 
-Lshapes.e <-rep(c(8,16,1))#shapes for legend
-#make the plot
-early.plot <- ordiplot(early.mds, choices=c(1,2), type = "none")   #Set up the plot
-points(spscoresall_early$NMDS1,spscoresall_early$NMDS2,col=cols1e$color,pch=shapes.e$shape) 
-plot(envvec.early, col="green")
-#text(early.mds, display = "species", cex=0.5, col="grey30") #label species
-legend("bottomright",legend=levels(as.factor(cols1e$thermal)), col=Lcolse, pch=15, cex=0.9,inset=0.1,bty="n",y.intersp=0.4,x.intersp=0.8,pt.cex=1.1)
-legend("topright",legend=levels(as.factor(shapes.e$time)), col="black", pch=Lshapes.e, cex=0.9,inset=0.03,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
-
-
-
-#########################################
 #separate NMDS plots by thermal designation
 #subset data by thermal
 cool<-data %>% subset (thermal=="cool") 
@@ -388,4 +321,137 @@ points(spscoresall.vwarm$NMDS1,spscoresall.vwarm$NMDS2,col=colsw,pch=shapes.vwar
 plot(envvec.nms.vwarm, col="green")
 text(spp.mds.vwarm, display = "species", cex=0.5, col="grey30") #label species
 legend("topright",legend=levels(as.factor(shapes.vwarm$time)), col="black", pch=Lshapesw, cex=0.9,inset=0.05,bty="n",y.intersp=0.25,x.intersp=0.4,pt.cex=1.1)
+
+#################################################################
+#######Subsetting MODERATE thermal by quadrat and year###########
+#################################################################
+
+## Set your datpath!! (file in Data_cleaning)
+
+library(tidyverse)
+library(readr)
+library(vegan)
+library(MASS)
+library(dplyr)
+library(RVAideMemoire) #for posthoc tests on permanova
+
+# Import csv file, transform to wide data, call it data
+dat <- read_csv(paste(datpath_clean, "/bugrdat.csv", sep=""))
+dat<-as.data.frame(dat)
+#dat2<-dat %>% mutate(cover=cover+0.00001) #a work around for data with lots of zeros
+data<- dat %>%
+  dplyr::select(-X1, -spcode) %>% 
+  separate(quadratNew, sep ="-", c("quadrat", "replicate")) %>%
+  spread(spname, cover)
+data[is.na(data)] <- 0 #replace NAs with 0 (species not counted in plots have NAs when wide dataset created)
+levels(as.factor(dat$quadratNew))
+levels(as.factor(dat$thermal))
+levels(as.factor(dat$spname)) #any to remove?
+
+#Import environmental data - NADP pinnacles deposition data and NOAA San Jose temperature data
+env <- read_csv(paste(datpath_clean, "/NTN-CA66-deposition.csv", sep=""))
+env<-env %>% rename(year=yr)
+temp <- read_csv(paste(datpath_clean, "/san_jose_clim.csv", sep=""),skip=9)
+temp <- temp %>% rename(year=DATE, temp=TAVG) %>% dplyr::select(year, temp)
+env<-merge(env,temp)
+
+#merge env to match full data
+all.env<-merge(env,data) %>% dplyr::select(NH4, NO3, totalN, ppt, temp)
+
+#check count of thermal factor
+data %>% 
+  group_by(thermal) %>%
+  summarise(no_rows = length(thermal)) #note very uneven
+
+str(data)
+
+data$ID <- seq.int(nrow(data))
+plotnames<-data[,1]
+cover.Bio<- data %>% dplyr::select(-c(1:4)) #wide data with ID columns removed, only species/cover for NMDS
+rownames(cover.Bio)<-plotnames
+
+#check for empty rows
+cover.Biodrop<-cover.Bio[rowSums(cover.Bio[, (1:157)]) ==0, ] #no empty rows, next step not needed
+#cover.Biodrop<-cover.Bio[rowSums(cover.Bio[, (1:157)])  >0 ]#remove empty rows
+
+
+#if needed, relativize by row or column or calculate presence/absence
+#cover.rowsums <- rowSums(cover.Bio [1:157])
+#cover.relrow <- data.frame(cover.Bio /cover.rowsums)
+#cover.colmax<-sapply(cover.Bio ,max)
+#cover.relcolmax <- data.frame(sweep(cover.Bio ,2,cover.colmax,'/'))
+#cover.pa <- cover.Bio %>% mutate_each(funs(ifelse(.>0,1,0)), 1:57)
+
+
+######################
+#2. NMS
+#see also section 2.1 of vegan tutorial: 
+#http://cc.oulu.fi/~jarioksa/opetus/metodi/vegantutor.pdf
+######################
+
+#make bray-curtis dissimilarity matrix
+spp.bcd <- vegdist(cover.Bio)
+
+#quick run to check out NMS ordination
+spp.mds0 <-isoMDS(spp.bcd) #runs nms only once
+spp.mds0  #by default 2 dimensions returned, stress is 6.4, converged
+ordiplot(spp.mds0) #ugly
+
+#prefer to run multiple NMS ordinations
+#with different starting configurations, and choose the best
+#this function does that, and in addition does several other steps too including: 
+#standardizing the data (though fuction call below turns this off with autotransform=F)
+#calculating distance matrix (default bray-curtis)
+#running NMDS with random starts
+#rotation of axes to maximize variance of site scores on axis 1
+#calculate species scores based on weighted averaging
+#help(metaMDS)
+spp.mds<-metaMDS(cover.Bio, trace = TRUE, autotransform=T, trymax=100, k=6) #runs several with different starting configurations
+#trace= TRUE will give output for step by step what its doing
+#default is 2 dimensions, can put k=4 for 4 dimensions
+spp.mds #solution did not converge after 100 tries
+summary(spp.mds)
+
+#quick plot of results
+stressplot(spp.mds, spp.bcd) #stressplot to show fit
+ordiplot(spp.mds)
+
+#overlay environmental variables on full data
+envvec.nms<-envfit(spp.mds,all.env, na.rm=TRUE)
+envvec.nms
+plot(spp.mds)
+plot(envvec.nms) #add vectors to previous ordination
+
+#store scores in new dataframe
+spscores1<-scores(spp.mds,display="sites",choices=1)
+spscores2<-scores(spp.mds,display="sites",choices=2)
+tplots<-data[,4]
+tplot_levels<-levels(tplots)
+spscoresall<-data.frame(tplots,spscores1,spscores2)
+
+#make nicer plot colored based on thermal, shapes on pre/post fire
+#help(ordiplot)
+#first, set colors and shapes
+cols1<- data %>% dplyr::select(thermal) %>% mutate(color = "black", 
+                                                   color = ifelse(thermal == "cool", "purple", 
+                                                                  ifelse(thermal=="moderate", "orange", 
+                                                                         ifelse(thermal=="very cool", "blue",
+                                                                                ifelse(thermal=="very warm", "red", color))))) #colors based on thermal group
+Lcols <- rep(c("Black", "Purple", "Orange", "Blue", "Red")) #colors for the legend
+shapes <- data %>% dplyr::select(year) %>%
+  mutate(shape = 1, shape = ifelse(year == "2004", 8, 
+                                   ifelse(year>="2005" & year<"2014", 16,
+                                          ifelse(year>="2014", 15,shape)))) #shapes based on year 
+shapes<-shapes %>% mutate(time="Pre-Fire", 
+                          time= ifelse(year==2004, "Fire",
+                                       ifelse(year>=2005&year<2014, "Post-Fire",
+                                              ifelse(year>=2014, "2014 and after",time)))) 
+Lshapes <-rep(c(15,8,16,1))#shapes for legend
+#make the plot
+bio.plot <- ordiplot(spp.mds, choices=c(1,2), type = "none")   #Set up the plot
+points(spscoresall$NMDS1,spscoresall$NMDS2,col=cols1$color,pch=shapes$shape) 
+plot(envvec.nms, col="green")
+text(spp.mds, display = "species", cex=0.5, col="grey30") #label species
+legend("bottomright",legend=levels(as.factor(cols1$thermal)), col=Lcols, pch=15, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
+legend("topright",legend=levels(as.factor(shapes$time)), col="black", pch=Lshapes, cex=0.9,inset=0.1,bty="n",y.intersp=0.5,x.intersp=0.8,pt.cex=1.1)
 
