a0fefb35992acc37c4305285a95e1f93ee403e40
Alejandro Brambila <abrambil@uoregon.edu>
2019-08-16 15:09:05 -0700

    shannon

--- Data_analysis/BUGR_timeseries.R
+++ Data_analysis/BUGR_timeseries.R
@@ -46,14 +46,26 @@ ggplot(richness, aes(year, mean_rich)) +
   geom_line(aes(color=as.factor(func)))+facet_wrap(~thermal) #+ geom_errorbar(aes((color=as.factor(func)), ymin=mean_rich-se_rich, ymax=mean_rich+se_rich), width=.2) +
   geom_line(aes(color=as.factor(func))) + ylab("mean species richness +/-se")
 
-#plot time series of shannon diversity
-shandiv<- community_diversity(dat1, time.var = "year", abundance.var="count", replicate.var="site.trt", metric = c("Shannon")) %>%
-  group_by(year, site, alltrt, Shannon)%>%
-  summarize(richness=length(unique(as.factor(code))))%>%
-  group_by(year, alltrt)%>%
-  summarize(meanrich=mean(richness), meanshan=mean(Shannon))
-
-ggplot(richsum)+geom_line(aes(as.factor(year), meanshan, group=alltrt, color=alltrt))
+  #plot time series of shannon diversity
+  dat3<-dat1%>%
+    filter(cover!=0)
+  dat3<-left_join(dat3, SC)%>%
+    mutate(sitetrt=paste(sitetrt, status, func, sep="_"))
+  simp<-community_diversity(dat3, time.var = "year", abundance.var="cover", replicate.var="sitetrt", metric = c("InverseSimpson"))
+  shandiv<- community_diversity(dat3, time.var = "year", abundance.var="cover", replicate.var="sitetrt", metric = c("Shannon")) 
+  
+  evenness<-left_join(simp, shandiv)%>%
+    separate(sitetrt, into=c("site", "trt", "status", "func"), sep="_")%>%
+    group_by(year, trt, status, func)%>%
+    summarize(meanShan=mean(Shannon), meanSimp=mean(InverseSimpson))%>%
+    filter(!is.na(status), !is.na(func), func!=("NA"), status!="NA")
+  
+  ggplot(evenness, aes(year, meanShan)) +
+    geom_line(aes(color=as.factor(interaction(status, func))))+facet_wrap(~trt)
+  
+  ggplot(evenness, aes(year, meanSimp)) +
+    geom_line(aes(color=as.factor(interaction(status, func))))+facet_wrap(~trt)
+  
 
 #join data and species key
 tog<-left_join(dat, SC)
