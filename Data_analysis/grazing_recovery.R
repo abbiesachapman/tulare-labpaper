4a7d5c1de6e9837200f6ef4260fbbc0f437485a5
Alejandro Brambila <abrambil@uoregon.edu>
2019-09-11 10:21:35 -0700

    starting rewrite for alldatsptrt instead of alldat sp and quadrat trt files like we had before

--- Data_analysis/grazing_recovery.R
+++ Data_analysis/grazing_recovery.R
@@ -3,7 +3,7 @@
 # 2006-2008 = ungrazed plots are ungrazed, 2009-2012 = ungrazed plots have cattle reintroduced
 # all years = grazed plots are grazed
 
-alldat<-read_csv(paste(datpath_clean, "/alldatsp.csv", sep="")) %>%
+alldat<-read_csv(paste(datpath_clean, "/alldatsptrt.csv", sep="")) #%>%
   select(-1)%>%
   group_by(year, spname, quadrat)%>%
   summarize(cover=sum(cover))%>%
@@ -16,7 +16,7 @@ treatments<-read_csv(paste(datpath_clean, "/quadrat_trt.csv", sep=""))%>%
   gather(key="year", value="data", -quadratID, -burn, -graze, -thermal)
 #treatments612 is only quadrats with data in 2006-2012
 treatments_612<-treatments%>%
-  filter(year==2006|year==2007|year==2008|year==2009|year==2010|year==2011|year==2012)%>%
+ # filter(year==2006|year==2007|year==2008|year==2009|year==2010|year==2011|year==2012)%>%
   mutate(year=paste("y", year, sep=""))%>%
   spread(year, data)%>%
   filter(!is.na(y2006),!is.na(y2007),!is.na(y2008),!is.na(y2009),!is.na(y2010),!is.na(y2011),!is.na(y2012))%>%
