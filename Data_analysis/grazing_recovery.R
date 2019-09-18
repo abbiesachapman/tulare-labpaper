d82b280def81709a94521a482a1064756a9667c8
Alejandro Brambila <abrambil@uoregon.edu>
2019-09-17 21:36:46 -0700

    richness double entries and comment out nonused lines

--- Data_analysis/grazing_recovery.R
+++ Data_analysis/grazing_recovery.R
@@ -20,7 +20,9 @@ rich <- alldat %>%
   mutate(trt=paste(graze, burn))%>%
   mutate(present=ifelse(cover>0, 1, 0))%>%
   group_by(year, quadratNew, func, trt, type, status, burn, graze, transect, present)%>%
-  summarize(richness = sum(present))
+  summarize(richness = sum(present))%>%
+  group_by(year, quadratNew, func, trt, type, status, burn, graze, transect)%>%
+  summarize(richness=sum(richness))
 
 rich1<-rich%>%
   group_by(year, trt, func, prepost) %>%
@@ -36,31 +38,31 @@ ggplot(rich1, aes(year, mean_rich)) +
 
 library(codyn)
 #plot time series of shannon diversity
-shan<-alldat%>%
-  filter(type!="NA", status!="NA")%>%
-  mutate(func=paste(type, status))%>%
-  mutate(trt=paste(graze, burn))%>%
-  filter(cover != 0, spname !="Unknown", spname!="Moss") %>%
-  mutate(alltrt=paste(quadratNew, trt, func, sep="_"))
-simp.g<-community_diversity(shan, time.var = "year", abundance.var="cover", replicate.var="alltrt", metric = c("InverseSimpson"))
-shandiv.g<- community_diversity(shan, time.var = "year", abundance.var="cover", replicate.var="alltrt", metric = c("Shannon")) 
+#shan<-alldat%>%
+#  filter(type!="NA", status!="NA")%>%
+#  mutate(func=paste(type, status))%>%
+#  mutate(trt=paste(graze, burn))%>%
+#  filter(cover != 0, spname !="Unknown", spname!="Moss") %>%
+#  mutate(alltrt=paste(quadratNew, trt, func, sep="_"))
+#simp.g<-community_diversity(shan, time.var = "year", abundance.var="cover", replicate.var="alltrt", metric = c("InverseSimpson"))
+#shandiv.g<- community_diversity(shan, time.var = "year", abundance.var="cover", replicate.var="alltrt", metric = c("Shannon")) 
 
-shan2<-left_join(simp.g, shandiv.g)%>%
-  separate(alltrt, into=c("transectNew", "trt", "func"), sep="_")%>%
-  mutate(prepost=ifelse(year<2009, "pre", "post"))
+#shan2<-left_join(simp.g, shandiv.g)%>%
+#  separate(alltrt, into=c("transectNew", "trt", "func"), sep="_")%>%
+#  mutate(prepost=ifelse(year<2009, "pre", "post"))
 
-shan1<-shan2%>%
-  group_by(year, trt, func, prepost)%>%
-  summarize(meanShan=mean(Shannon), seShan=calcSE(Shannon), meanSimp=mean(InverseSimpson))%>%
-  filter(!is.na(trt), !is.na(func), func!=("NA"), trt!="NA")
+#shan1<-shan2%>%
+#  group_by(year, trt, func, prepost)%>%
+#  summarize(meanShan=mean(Shannon), seShan=calcSE(Shannon), meanSimp=mean(InverseSimpson))%>%
+#  filter(!is.na(trt), !is.na(func), func!=("NA"), trt!="NA")
 
-ggplot(shan1, aes(year, meanShan)) +
-  geom_line(aes(color=as.factor(trt)))+
-  geom_point(aes(color=as.factor(trt)))+
-  facet_wrap(~func) +
-  geom_errorbar(aes(ymin=meanShan-seShan, ymax=meanShan+seShan, color=as.factor(trt)), width=.2)+
-  geom_vline(xintercept=2008.5)+geom_vline(xintercept=2004.5, color="red")+ylab("Shannon Diversity") +
-  labs(x = "Year", y = "Mean Shannon Diversity", color = "Treatment")
+#ggplot(shan1, aes(year, meanShan)) +
+#  geom_line(aes(color=as.factor(trt)))+
+#  geom_point(aes(color=as.factor(trt)))+
+#  facet_wrap(~func) +
+#  geom_errorbar(aes(ymin=meanShan-seShan, ymax=meanShan+seShan, color=as.factor(trt)), width=.2)+
+#  geom_vline(xintercept=2008.5)+geom_vline(xintercept=2004.5, color="red")+ylab("Shannon Diversity") +
+#  labs(x = "Year", y = "Mean Shannon Diversity", color = "Treatment")
 
 #plot timeseries of cover
 cov<-alldat%>%
@@ -94,11 +96,11 @@ ggplot(cov1, aes((year), meanrelcov))+
   facet_wrap(~func) +geom_vline(xintercept=2008.5)+geom_vline(xintercept=2004.5, color="red") +
   labs(x = "Year", y = "Mean Relative Cover", color = "Treatment")
 
-#raw data
-ggplot(cov, aes((year), relcov))+
-  geom_point(aes(color=trt))+
-  facet_wrap(~func) +geom_vline(xintercept=2008.5)+geom_vline(xintercept=2004.5, color="red") +
-  labs(x = "Year", y = "Relative Cover", color = "Treatment")
+##raw data
+#ggplot(cov, aes((year), relcov))+
+#  geom_point(aes(color=trt))+
+#  facet_wrap(~func) +geom_vline(xintercept=2008.5)+geom_vline(xintercept=2004.5, color="red") +
+#  labs(x = "Year", y = "Relative Cover", color = "Treatment")
 
 ##########
 #Litter
@@ -209,32 +211,32 @@ ggarrange(f1, f2, f3, f4, f5, f6,  ncol = 2, nrow = 3,
 ##########
 # Indicator Species
 ###########
-library(indicspecies)
-grztog3<-grztog2%>%
-  mutate(prepost=ifelse(year<2009, "pre", "post"))%>%
-  mutate(trtgroup=paste(graze, prepost, sep="_"))%>%
-  mutate(rep=paste(transect.quad, year))%>%
-  select(-transect, -transect.quad,-year, -quadrat, -graze, -prepost, -thermal, -spname, -status, -func)
+#library(indicspecies)
+#grztog3<-grztog2%>%
+#  mutate(prepost=ifelse(year<2009, "pre", "post"))%>%
+#  mutate(trtgroup=paste(graze, prepost, sep="_"))%>%
+#  mutate(rep=paste(transect.quad, year))%>%
+#  select(-transect, -transect.quad,-year, -quadrat, -graze, -prepost, -thermal, -spname, -status, -func)
 
-indic_treatments<-select(grztog3, 3, 4)%>%
-  unique()
-indic_species<-select(grztog3, 1, 2, 4)%>%
-  spread(spcode, cover, fill=0)%>%
-  remove_rownames()%>%
-  column_to_rownames("rep")
-indic_species<-decostand(indic_species, "total")
+#indic_treatments<-select(grztog3, 3, 4)%>%
+#  unique()
+#indic_species<-select(grztog3, 1, 2, 4)%>%
+#  spread(spcode, cover, fill=0)%>%
+##  remove_rownames()%>%
+#  column_to_rownames("rep")
+#indic_species<-decostand(indic_species, "total")
 
-indic_treatments2<-indic_species%>%
-  rownames_to_column("rep")%>%
-  select(1)
-indic_treatments3<-left_join(indic_treatments2, indic_treatments)
+#indic_treatments2<-indic_species%>%
+#  rownames_to_column("rep")%>%
+#  select(1)
+#indic_treatments3<-left_join(indic_treatments2, indic_treatments)
 
-indicators<-multipatt(indic_species, indic_treatments$trtgroup, func="IndVal.g", control=how(nperm=999))
+#indicators<-multipatt(indic_species, indic_treatments$trtgroup, func="IndVal.g", control=how(nperm=999))
 
-indsum<-indicators$sign%>%
-  rownames_to_column("species")%>%
-  filter(p.value<.05)
-indsum1<-left_join(indsum, SC, by=c("species"="spcode"))%>%
-  select(-index, -stat, -p.value)
+#indsum<-indicators$sign%>%
+#  rownames_to_column("species")%>%
+#  filter(p.value<.05)
+#indsum1<-left_join(indsum, SC, by=c("species"="spcode"))%>%
+#  select(-index, -stat, -p.value)
 
-write.csv(indsum1, file = "Grazing_indicator_sp.csv")
+#write.csv(indsum1, file = "Grazing_indicator_sp.csv")
